645f37bd0d03990cbb9eab108e4e21027da5437c
Kamil Pirog <kamilpirog98@gmail.com>
2022-01-25 17:11:34 +0100

    Ball movement

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed = 5.0f;
+    
+    private Rigidbody2D _rigidbody;
+
+    private void Awake() => _rigidbody = GetComponent<Rigidbody2D>();
+
+    private void Start()
+    {
+        AddRandomForce();
+    }
+
+    private void AddRandomForce()
+    {
+        float x = Random.value < 0.5f ? UnityEngine.Random.Range(1.0f, -1.0f) : UnityEngine.Random.Range(-1.0f, 1.0f);
+        float y = Random.value < 0.5f ? -1.0f : 1.0f;
+
+        Vector2 forceDirection = new Vector2(x, y);
+        _rigidbody.AddForce(forceDirection * _movementSpeed);
+    }
+}
